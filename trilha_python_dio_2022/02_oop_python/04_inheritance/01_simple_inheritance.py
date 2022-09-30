0efa251917422d2b55f95dded2d38614c55f5921
AngelOttoni <angel_m_ottoni@hotmail.com>
2022-09-30 09:42:01 -0300

    print formatted

--- trilha_python_dio_2022/02_oop_python/04_inheritance/01_simple_inheritance.py
+++ trilha_python_dio_2022/02_oop_python/04_inheritance/01_simple_inheritance.py
@@ -1,37 +1,47 @@
-class Veiculo:
-    def __init__(self, cor, placa, numero_rodas):
-        self.cor = cor
-        self.placa = placa
-        self.numero_rodas = numero_rodas
+class Vehicle:
+    def __init__(self, color, license_plate, number_wheels):
+        self.color = color
+        self.license_plate = license_plate 
+        self.number_wheels = number_wheels
 
-    def ligar_motor(self):
-        print("Ligando o motor")
+    def start_engine(self):
+        print("Starting the engine")
 
     def __str__(self):
-        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+        return f"{self.__class__.__name__}: {', '.join([f'{key} = {value}' for key, value in self.__dict__.items()])}"
 
 
-class Motocicleta(Veiculo):
+class Motorcycle(Vehicle):
     pass
 
 
-class Carro(Veiculo):
+class Car(Vehicle):
     pass
 
 
-class Caminhao(Veiculo):
-    def __init__(self, cor, placa, numero_rodas, carregado):
-        super().__init__(cor, placa, numero_rodas)
-        self.carregado = carregado
+class Truck(Vehicle):
+    def __init__(self, color, license_plate, number_wheels, loaded):
+        super().__init__(color, license_plate, number_wheels)
+        self.loaded = loaded
 
-    def esta_carregado(self):
-        print(f"{'Sim' if self.carregado else 'Não'} estou carregado")
+    def is_loaded(self):
+        print(f"{'Yes' if self.loaded else 'No'} I'm loaded")
 
 
-moto = Motocicleta("preta", "abc-1234", 2)
-carro = Carro("branco", "xde-0098", 4)
-caminhao = Caminhao("roxo", "gfd-8712", 8, True)
+motorcycle = Motorcycle("black", "abc-1234", 2)
+motorcycle.start_engine()
 
-print(moto)
-print(carro)
-print(caminhao)
+car = Car("white", "xde-0098", 4)
+car.start_engine()
+
+truck = Truck("purple", "gfd-8712", 8, True)
+truck.start_engine()
+print("")
+
+print(f"Morocycle description: Color - {motorcycle.color}, License Plate - {motorcycle.license_plate}, Number of wheels - {motorcycle.number_wheels}.")
+print("")
+
+print(f"Car description: Color - {car.color}, License Plate - {car.license_plate}, Number of wheels - {car.number_wheels}.")
+print("")
+
+print(f"Truck description: Color - {truck.color}, License Plate - {truck.license_plate}, Number of wheels - {truck.number_wheels}, Loaded - {truck.loaded}.")
