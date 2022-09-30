bd4aca7c5f35bf736dd78f7d55ec7a42447829e7
AngelOttoni <angel_m_ottoni@hotmail.com>
2022-09-30 12:13:36 -0300

    full_Name implemented

--- trilha_python_dio_2022/02_oop_python/05_encapsulation/03_example_properties_person.py
+++ trilha_python_dio_2022/02_oop_python/05_encapsulation/03_example_properties_person.py
@@ -1,13 +1,39 @@
-class Pessoa:
-    def __init__(self, nome, ano_nascimento):
-        self.nome = nome
-        self._ano_nascimento = ano_nascimento
+# class Person:
+#     def __init__(self, name, birth_date):
+#         self.name = name
+#         self._birth_date = birth_date
+    
+    
 
+#     @property
+#     def age(self):
+#         _current_year = 2022
+#         return _current_year - self._birth_date
+
+
+# person1 = Person("Anna", 1994)
+# print(f"Name: {person1.name} \tAge: {person1.age}")
+
+
+class Person:
+    def __init__(self, first_name, middle_name, last_name, year_birth):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self._year_birth = year_birth
+    
     @property
-    def idade(self):
-        _ano_atual = 2022
-        return _ano_atual - self._ano_nascimento
+    def full_Name(self):
+        return f"{self.first_name} {self.middle_name} {self.last_name}"
+        
+    @property
+    def age(self):
+        _current_year = 2022
+        return _current_year - self._year_birth
 
 
-pessoa = Pessoa("Guilherme", 1994)
-print(f"Nome: {pessoa.nome} \tIdade: {pessoa.idade}")
+person1 = Person("Anna", "Lima", "Costa", 1994)
+print(f"Full Name: {person1.full_Name} \tAge: {person1.age}")
+
+person2 = Person("Angelo", "Lima", "Costa", 1994)
+print(f"Full Name: {person2.full_Name} \tAge: {person2.age}")
