466c2f20f78c631b3b5396ffb24e0fb880416acb
AngelOttoni <angel_m_ottoni@hotmail.com>
2022-09-30 09:57:55 -0300

    print formatted

--- trilha_python_dio_2022/02_oop_python/04_inheritance/02_multiple_inheritance.py
+++ trilha_python_dio_2022/02_oop_python/04_inheritance/02_multiple_inheritance.py
@@ -1,34 +1,36 @@
 class Animal:
-    def __init__(self, nro_patas):
-        self.nro_patas = nro_patas
+    def __init__(self, num_legs):
+        self.num_legs = num_legs
 
     def __str__(self):
-        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+        return f"{self.__class__.__name__}: {', '.join([f'{key} = {value}' for key, value in self.__dict__.items()])}"
 
 
-class Mamifero(Animal):
-    def __init__(self, cor_pelo, **kw):
-        self.cor_pelo = cor_pelo
+class Mammal(Animal):
+    def __init__(self, fur_color, **kw):
+        #cor do pelo
+        self.fur_color = fur_color
         super().__init__(**kw)
 
 
 class Ave(Animal):
-    def __init__(self, cor_bico, **kw):
-        self.cor_bico = cor_bico
+    def __init__(self, color_beak, **kw):
+        self.color_beak = color_beak
         super().__init__(**kw)
 
 
-class Gato(Mamifero):
+class Cat(Mammal):
     pass
 
 
-class Ornitorrinco(Mamifero, Ave):
-    def __init__(self, cor_bico, cor_pelo, nro_patas):
-        super().__init__(cor_pelo=cor_pelo, cor_bico=cor_bico, nro_patas=nro_patas)
+class Ornithorhynchus(Mammal, Ave):
+    def __init__(self, color_beak, fur_color, num_legs):
+        super().__init__(fur_color=fur_color, color_beak=color_beak, num_legs=num_legs)
 
+print("")
+cat = Cat(num_legs=4, fur_color="black")
+print(f"Cat description: Fur color = {cat.fur_color}, Number of legs = {cat.num_legs}.")
 
-gato = Gato(nro_patas=4, cor_pelo="Preto")
-print(gato)
-
-ornitorrinco = Ornitorrinco(nro_patas=2, cor_pelo="vermelho", cor_bico="laranja")
-print(ornitorrinco)
+print("")
+ornithorhynchus = Ornithorhynchus(color_beak="orange", fur_color="red", num_legs=2)
+print(f"Ornithorhynchus description: Beak color = {ornithorhynchus.color_beak}, Fur color = {ornithorhynchus.fur_color}, Number of legs = {ornithorhynchus.num_legs}.")
